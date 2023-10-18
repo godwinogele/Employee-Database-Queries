09467d20e704b07e7adb8022e7be2a195e90fb3c
Godwin Agwu <82897661+godwinogele@users.noreply.github.com>
2023-10-18 16:52:57 +0100

    Update Retrieving employee details.sql

--- Retrieving employee details.sql
+++ Retrieving employee details.sql
@@ -2,39 +2,39 @@
 
 ---Retrieving data from Employee details table
 Select*
-From Employee_Details
+From Employee_Details;
 
 ---Retrieving EmployeeID, FirstName, Age, Gender data from Employee details table
 Select EmployeeID, FirstName, Age, Gender
-from Employee_Details
+from Employee_Details;
 
 ---Youngest employees
 Select EmployeeID, FirstName, Age, Gender
 from Employee_Details
-where age = (select min (age) from Employee_Details)
+where age = (select min (age) from Employee_Details);
 
 ---Oldest employees
 Select EmployeeID, FirstName, Age, Gender
 from Employee_Details
-where age = (select max (age) from Employee_Details)
+where age = (select max (age) from Employee_Details);
 
 ---Number of columns in employee details tables
 Select count(*) as number_of_employees
-From Employee_Details
+From Employee_Details;
 
 ---Highest salary
 Select max (salary) as highest_paid
-from Employee_Salary
+from Employee_Salary;
 
 ---Lowest salary
 select min (salary) as lowest_paid
-from Employee_Salary
+from Employee_Salary;
 
 ---Average salary
 select avg (salary) as average_salary
-from Employee_Salary
+from Employee_Salary;
 
 ---Staff who are less than 30years
 select count(*) age
 from Employee_Details
-where age > 30
+where age > 30;
